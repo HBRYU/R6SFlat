96a41940ec3779e037a7864718c2a5e52c718d0b
HBRYU <48374744+HBRYU@users.noreply.github.com>
2020-08-23 20:00:54 +0900

    8/23
    텍스쳐
    문

---
+++ R6S Flat/Assets/ALL/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool open;
+    public float accessDistance;
+    public Transform doorPos;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((doorPos != null) && (UTIL.FastDist(UTIL.GetPlayer().transform.position, doorPos.position, 0.1f) <= accessDistance) && Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("open", !open);
+            open = !open;
+        }
+    }
+}
